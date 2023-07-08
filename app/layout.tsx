13f9c7e747872a3147d11d2f09ca0efdeee22a1d
ishaan1013 <ishaan1013@gmail.com>
2023-07-08 03:13:06 -0400

    clerk initial auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/themeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Nav from "@/components/ui/nav";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, auth } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,21 +18,35 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { userId } = auth();
+
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
           {" "}
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <>
+            {userId ? (
+              <>
+                <main className="w-screen flex justify-center bg-neutral-950">
+                  <Nav />
+                  <div className="min-h-screen text-base w-full max-w-[500px] bg-neutral-950 relative pb-14">
+                    {children}
+                  </div>
+                </main>
+                <Toaster />
+              </>
+            ) : (
               <main className="w-screen flex justify-center bg-neutral-950">
-                <Nav />
-                <div className="min-h-screen text-base w-full max-w-[500px] bg-neutral-950 relative pb-14">
+                <div className="min-h-screen flex flex-col justify-center items-center text-base w-full max-w-[500px] bg-neutral-950 relative">
                   {children}
                 </div>
               </main>
-              <Toaster />
-            </>
+            )}
           </ThemeProvider>
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,41 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import Item from "@/components/thread";
 import logo from "@/assets/threads.svg";
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
-  return (
-    // <main className="w-screen flex justify-center bg-black">
-    //   <Nav />
+import { auth } from "@clerk/nextjs";
 
-    //   <div className="min-h-screen text-base w-full max-w-[500px] bg-black relative pb-14">
+export default function Page() {
+  const { userId } = auth();
+
+  if (!userId)
+    return (
+      <>
+        <div className="h-16 w-16 bg-cover">
+          <Image
+            src={logo}
+            alt="Threads logo"
+            className="min-h-full invert min-w-full object-cover"
+          />
+        </div>
+        <div className="gradient mt-4 mb-12 text-4xl font-bold">Threads</div>
+
+        <Link href="/sign-up" className="w-full px-6">
+          <Button className="w-full" variant="outline">
+            Create Your Account
+          </Button>
+        </Link>
+        <Link href="/sign-in" className="w-full px-6 mt-2">
+          <Button className="w-full" variant="ghost">
+            Sign In
+          </Button>
+        </Link>
+      </>
+    );
+
+  return (
     <>
       <div className="flex items-center justify-center w-full py-5">
         <div className="h-9 w-9 bg-cover">
